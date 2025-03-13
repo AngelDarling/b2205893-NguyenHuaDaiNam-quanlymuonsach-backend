4db4bafc52510fc316abd10ecdafbc3691576f12
AngelDarling <nguyenhuadainam@gmail.com>
2025-03-13 19:16:01 +0700

    Cai dat express

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+// const contactsRouter = require("./app/routes/contact.route");
+// const ApiError = require("./app/api-error");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
+});
+
+// app.use("/api/contacts", contactsRouter);
+
+// // Middleware xử lý lỗi 404
+// app.use((req, res, next) => {
+//   next(new ApiError(404, "Resource not found"));
+// });
+
+// // Middleware xử lý lỗi tập trung
+// app.use((err, req, res, next) => {
+//   res.status(err.statusCode || 500).json({
+//     message: err.message || "Internal Server Error",
+//   });
+// });
+
+module.exports = app;
